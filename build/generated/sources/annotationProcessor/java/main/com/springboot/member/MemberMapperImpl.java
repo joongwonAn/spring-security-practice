b9087ae9ab5df8001962f2570d56bcee396b82ff
joongwon <anjoongwon517@gmail.com>
2025-09-24 13:03:36 +0900

    Initial commit

---
+++ build/generated/sources/annotationProcessor/java/main/com/springboot/member/MemberMapperImpl.java
@@ -0,0 +1,28 @@
+package com.springboot.member;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-24T11:39:32+0900",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 17.0.15 (Microsoft)"
+)
+@Component
+public class MemberMapperImpl implements MemberMapper {
+
+    @Override
+    public Member memberPostToMember(MemberDto.Post requestBody) {
+        if ( requestBody == null ) {
+            return null;
+        }
+
+        Member member = new Member();
+
+        member.setFullName( requestBody.getFullName() );
+        member.setEmail( requestBody.getEmail() );
+        member.setPassword( requestBody.getPassword() );
+
+        return member;
+    }
+}
