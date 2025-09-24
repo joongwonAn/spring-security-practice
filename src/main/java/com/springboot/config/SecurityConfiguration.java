26151beddc3d6b2bb35954c65989b44f7b7b65fb
joongwon <anjoongwon517@gmail.com>
2025-09-24 13:41:07 +0900

    admin 권한 추가

---
+++ src/main/java/com/springboot/config/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/auths/login-form")
+                .loginProcessingUrl("/process_login")
+                .failureUrl("/auths/login-form?error")
+                .and()
+                .logout() // logout 기능 추가
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling().accessDeniedPage("/auths/access-denied")
+                .and()
+                .authorizeHttpRequests(authorize -> authorize                  // (2)
+                        .antMatchers("/orders/**").hasRole("ADMIN")
+                        .antMatchers("/members/my-page").hasRole("USER")
+                        .antMatchers("/**").permitAll()
+                );
+                /*.authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();     */ // 모든 권한을 준 경우
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailService() {
+        UserDetails userDetails =
+                User.withDefaultPasswordEncoder()
+                        .username("jw@gmail.com")
+                        .password("1111")
+                        .roles("USER")
+                        .build();
+
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin@gmail.com")
+                        .password("2222")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(userDetails, admin);
+    }
+}
