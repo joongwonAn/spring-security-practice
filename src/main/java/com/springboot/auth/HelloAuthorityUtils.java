f4b3aeac66dcc1c27874ba112608ab363150e5e3
joongwon <anjoongwon517@gmail.com>
2025-09-25 09:42:34 +0900

    HelloUserDetailsService에서 Role 기반의 User 권한을 생성하기 위해 사용한 HelloAuthorityUtils 코드

---
+++ src/main/java/com/springboot/auth/HelloAuthorityUtils.java
@@ -0,0 +1,25 @@
+package com.springboot.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class HelloAuthorityUtils {
+    @Value("${mail.address.admin}") // yml에 정의한 프로퍼티를 가져옴
+    private String adminMailAddress;
+
+    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
+
+    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
+
+    public List<GrantedAuthority> createAuthorities(String email) {
+        if (email.equals(adminMailAddress)) {
+            return ADMIN_ROLES;
+        }
+        return USER_ROLES;
+    }
+}
