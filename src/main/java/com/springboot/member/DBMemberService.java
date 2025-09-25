723a6f4683daa9a9fb9d73f4a03077cbf58141f9
joongwon <anjoongwon517@gmail.com>
2025-09-25 09:31:13 +0900

    회원 정보를 DB에 저장

--- src/main/java/com/springboot/member/DBMemberService.java
+++ src/main/java/com/springboot/member/DBMemberService.java
@@ -1,12 +1,33 @@
 package com.springboot.member;
 
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class DBMemberService implements MemberService {
+    private MemberRepository memberRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public DBMemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public Member createMember(Member member) {
-        return null;
+        verifyExistsEmail(member.getEmail());
+        String encodedPassword = passwordEncoder.encode(member.getPassword());
+
+        member.setPassword(encodedPassword);
+
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+
+    private final void verifyExistsEmail(String email) {
+        memberRepository.findByEmail(email)
+                .ifPresent(exists -> new BusinessLogicException(ExceptionCode.MEMBER_EXISTS));
     }
 }
